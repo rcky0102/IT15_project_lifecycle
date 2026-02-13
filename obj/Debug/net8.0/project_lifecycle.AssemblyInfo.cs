8cb057aa05c324032ee230e3040d7532a9b507f7
rcky0102 <rocky.adaya101@gmail.com>
2026-02-13 19:43:35 +0800

    Checkpoint

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a413ed7b35579d600b6940cfd245ecc97961572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53048617ce41a74ae770cb825303dc25f9fe086")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
