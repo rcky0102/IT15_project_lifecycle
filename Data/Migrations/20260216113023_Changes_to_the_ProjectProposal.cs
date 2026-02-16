dfd0fd74f117330a10e67a39d7a763c283eb6fe4
rcky0102 <rocky.adaya101@gmail.com>
2026-02-16 19:39:54 +0800

    Making Project Proposal work

---
+++ Data/Migrations/20260216113023_Changes_to_the_ProjectProposal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project_lifecycle.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changes_to_the_ProjectProposal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "ProjectProposals");
+
+            migrationBuilder.DropColumn(
+                name: "FileAttachment",
+                table: "ProjectProposals");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "ProjectProposals");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "ProjectProposals",
+                newName: "Input");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Input",
+                table: "ProjectProposals",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "ProjectProposals",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileAttachment",
+                table: "ProjectProposals",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "ProjectProposals",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
