107f35c90058a73b3b301effc02ec05dfa774ff7
rcky0102 <rocky.adaya101@gmail.com>
2026-02-24 11:11:56 +0800

    dashoard UI

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2a37528104e668a3fb44c77e2760424221ba8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5644be4708094c1ea29a692d0f06f60493436dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
