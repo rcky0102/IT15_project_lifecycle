5ca7c4b640e10ccfc12608e855997c6a1ba941ea
rcky0102 <rocky.adaya101@gmail.com>
2026-02-25 21:23:28 +0800

    Proposal (2)

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7f47991e0be2705f8042107b19999a3a4c7bcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af480cd7b39602df5611c5bf592c1135ac1e726")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
