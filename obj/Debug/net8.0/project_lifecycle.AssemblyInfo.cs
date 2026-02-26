8f3ac9d39672ac67d5992747cc2ea13c4b88ca93
rcky0102 <rocky.adaya101@gmail.com>
2026-02-26 19:41:12 +0800

    management

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48616e1d321f025860e17795fffe70a0c83dc1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3d64b8659db8f77a24d5eadaa574477bfc354e")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
