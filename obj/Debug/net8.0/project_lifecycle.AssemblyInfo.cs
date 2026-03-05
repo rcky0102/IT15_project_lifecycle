5303bda3374c9b236e148e064291dfcc015949b8
Rocky Adaya <167247708+rcky0102@users.noreply.github.com>
2026-03-06 07:33:37 +0800

    Confirmation dialogs (1)

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9ef1d173df654ef459a5c0a510ff6772543565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df0d566e5f547583f2f0f68883199573be7f6bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
