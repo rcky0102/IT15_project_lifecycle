f32267de8be1a32b7e4756ae846ce188f64066c6
Rocky Adaya <167247708+rcky0102@users.noreply.github.com>
2026-03-06 20:29:50 +0800

    redesign (3)

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fc6a15bb33487c32f03043961f508449ce2b0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67224f6a8da202d5cbaeabd5ed1ece919744d4d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
