bc97bebce39b9eb3885e5dd9e93bb623d5b3c950
Rocky Adaya <167247708+rcky0102@users.noreply.github.com>
2026-03-06 10:06:45 +0800

    From modal to actual create page (1)

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9b6e6c2165450a7236e3759cb25f8b68dbe2bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8650b32d6cc1784029a6222c9ecd079500cb861d")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
