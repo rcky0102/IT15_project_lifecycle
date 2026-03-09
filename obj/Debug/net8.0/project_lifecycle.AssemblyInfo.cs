c80586ef2089bcecd707aaa058ed31e40ccda93b
Rocky Adaya <167247708+rcky0102@users.noreply.github.com>
2026-03-09 22:44:23 +0800

    Polish (10)

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9e80b95dbe01db4b28d5aec2effa00a19dd9dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d4189e820c7860c84ea0cffc2699d9d3c0dc430")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
