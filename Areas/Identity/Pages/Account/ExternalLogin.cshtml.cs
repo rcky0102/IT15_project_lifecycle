3622ef1d692325b3216022dd8e2aa5647d395371
Rocky Adaya <167247708+rcky0102@users.noreply.github.com>
2026-05-02 11:42:29 +0800

    legit checkpoint

---
+++ Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project_lifecycle.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ExternalLoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ExternalLoginModel(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public string Provider { get; set; }
+
+        public IActionResult OnPost(string provider, string returnUrl = null)
+        {
+            // Request a redirect to the external login provider.
+            var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            return new ChallengeResult(provider, properties);
+        }
+
+        public async Task<IActionResult> OnGetCallbackAsync(string returnUrl = null, string remoteError = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if (remoteError != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            // Sign in the user with this external login provider if the user already has a login.
+            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+            if (result.Succeeded)
+            {
+                // Get the user and determine role
+                var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+                if (user != null)
+                {
+                    var roles = await _userManager.GetRolesAsync(user);
+                    var role = roles.FirstOrDefault() ?? string.Empty;
+
+                    // Map role to area/dashboard
+                    var redirect = role switch
+                    {
+                        "Employee" => Url.Content("~/Employee/Dashboard"),
+                        "ProjectManager" => Url.Content("~/ProjectManager/Dashboard"),
+                        "DepartmentHead" => Url.Content("~/DepartmentHead/Dashboard"),
+                        "HumanResource" => Url.Content("~/HumanResource/Dashboard"),
+                        "Executive" => Url.Content("~/Executive/Dashboard"),
+                        "SuperAdmin" => Url.Content("~/SuperAdmin/Dashboard"),
+                        "Admin" => Url.Content("~/Admin/Dashboard"),
+                        _ => returnUrl
+                    };
+
+                    return LocalRedirect(redirect);
+                }
+
+                return LocalRedirect(returnUrl);
+            }
+
+            // If the user does not have an account, the ExternalLogin flow in this app
+            // may rely on other flows (register/import). For now redirect to returnUrl.
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
--- obj/Release/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Release/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e815588d00e9798fe9e5d23100c23e3a38c6b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d229834d03abd9718f0a36c480a601c4c61310b")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
