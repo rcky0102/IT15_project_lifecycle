1242dbed26002853474282526b385a31d690f0a4
Rocky Adaya <167247708+rcky0102@users.noreply.github.com>
2026-05-04 09:37:12 +0800

    another security logs attempt (5)

--- obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_lifecycle.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1847d10fb920facb613e1e8dad2ed70b0d687e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82601b705ce3bec12fa9297b7b2de91e23d34dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_lifecycle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
